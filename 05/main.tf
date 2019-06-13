c7208d448d4bf1700bbca8689aefc5affbb6fb0c
suaaa7 <spuaaa7@gmail.com>
2019-06-14 04:00:32 +0900

    add iam_role module

--- 05/main.tf
+++ 05/main.tf
@@ -1,3 +1,10 @@
+module "describe_region_for_ec2" {
+  source     = "./iam_role"
+  name       = "describe-regions-for-ec2"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.allow_describe_regions.json
+}
+
 data "aws_iam_policy_document" "allow_describe_regions" {
   statement {
     effect    = "Allow"
