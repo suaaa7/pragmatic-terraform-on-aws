10882ae04067baab6a12ff30e15e1fd478c5e49c
suaaa7 <spuaaa7@gmail.com>
2019-06-27 03:37:39 +0900

    add ecr_lifecycle_policy

--- Batch/ecr.tf
+++ Batch/ecr.tf
@@ -2,3 +2,13 @@ resource "aws_ecr_repository" "ecr" {
   name = "batch-repository"
 }
 
+resource "aws_ecr_lifecycle_policy" "ecr" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = data.template_file.ecr.rendered
+}
+
+data "template_file" "ecr" {
+  template = file("json/ecr_lifecycle_policy.json")
+}
+
