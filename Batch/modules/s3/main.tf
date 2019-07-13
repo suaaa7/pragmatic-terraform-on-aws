3b63fd53d308851a8700cff9d4127b7fecd82a8b
suaaa7 <spuaaa7@gmail.com>
2019-07-13 21:01:18 +0900

    Add aws_s3_bucket_public_access_block

--- Batch/modules/s3/main.tf
+++ Batch/modules/s3/main.tf
@@ -1,9 +1,17 @@
 variable "bucket_name" {}
 
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "private" {
   bucket = var.bucket_name
   acl = "private"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "private" {
+  bucket = aws_s3_bucket.private.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "bucket" {
