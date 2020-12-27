a6ce59e39fac2deb8415732d08c76c8fd0f978dd
suaaa7 <spuaaa7@gmail.com>
2020-12-27 18:37:55 +0900

    Implemented up to Chapter 15

--- code/cloudwatch.tf
+++ code/cloudwatch.tf
@@ -32,3 +32,8 @@ resource "aws_cloudwatch_event_target" "example_batch" {
     }
   }
 }
+
+resource "aws_cloudwatch_log_group" "operation" {
+  name              = "/operation"
+  retention_in_days = 30
+}
